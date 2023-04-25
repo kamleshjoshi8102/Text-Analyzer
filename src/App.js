71b4634270f14bc975e5510f6dedb3c52bdc638a
kamleshjoshi8102 <j.kamlesh8102@gmail.com>
2023-04-25 21:03:16 +0530

    dark-theme

--- src/App.js
+++ src/App.js
@@ -1,15 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
-
+import About from "./components/About";
+import Alert from "./components/Alert";
 
 function App() {
+  const [mode,setMode] = useState('light');
+  const[alert,setAlert] = useState(null);
+  const toggleMode = () =>{
+    if(mode=='light'){
+      setMode("dark");
+      document.body.style.backgroundColor="#49484a";
+      document.body.style.color="white";
+      showAlert("Dark mode has been Enabled","success");
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor="white";
+      document.body.style.color="black";
+      showAlert("Light mode has been Enabled","success");
+    }
+  }
+  
+  const showAlert = (message,type)=>{
+    setAlert({
+      msg:message,
+      type:type
+  })
+  setTimeout(()=>{
+    setAlert(null);
+  },1500)
+  }
   return (
     <>
-      <Navbar title="Text Analyzer"/>
+      <Navbar title="Text Analyzer" mode={mode} toggleMode = {toggleMode} />
+      <Alert alert={alert}/>
       <div className="container">
-        <TextForm heading = "Enter Text To Analyze" />
-      </div>
+         <TextForm showAlert={showAlert} heading = "Enter Text To Analyze" />
+  {/*<About/>*/}
+      </div> 
     </>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,10 @@ import PropTypes from "prop-types";
 
 export default function Navbar(props) {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav
+      className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}
+      style={{ paddingRight:"10px" , paddingLeft:"10px"}}
+    >
       <a className="navbar-brand" href="/">
         {props.title}
       </a>
@@ -23,19 +26,26 @@ export default function Navbar(props) {
         <ul className="navbar-nav mr-auto">
           <li className="nav-item active">
             <a className="nav-link" href="/">
-              Home <span className="sr-only">(current)</span>
+              Home
             </a>
           </li>
         </ul>
       </div>
+      <div
+        className={`form-check form-switch text-${
+          props.mode === "light" ? "dark" : "light"
+        }`}
+      >
+        <input
+          className="form-check-input"
+          type="checkbox"
+          id="flexSwitchCheckDefault"
+          onClick={props.toggleMode}
+        />
+        <label className="form-check-label" htmlFor="flexSwitchCheckDefault">
+          Enable {props.mode === "light" ? "Dark" : "Light"} Mode
+        </label>
+      </div>
     </nav>
   );
 }
-
-Navbar.propTypes = {
-  title: PropTypes.string,
-};
-
-Navbar.defaultProps = {
-  title: "Navbar",
-};
